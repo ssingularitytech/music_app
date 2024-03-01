371c1097fa1b26ab40d29ff80b14b7161e24877f
sandeepspd123 <sandeepsky708@gmail.com>
2024-03-01 13:33:11 +0530

    git clone music_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   get 'landing/index'
   get 'landing/instrument'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
